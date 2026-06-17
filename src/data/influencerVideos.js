0cdc22396a840c8299560f0bb15c6ad5cbcf5aa9
Harishkrishnakumarr <harishkrishnakumarr@gmail.com>
2026-06-17 14:08:31 +0530

    video deleted

--- src/data/influencerVideos.js
+++ src/data/influencerVideos.js
@@ -8,6 +8,5 @@ export const videos = [
   { id: 7, title: 'Food vlog', thumbnail: '/thumbnail/influencers/palaya.png', videoUrl: "https://res.cloudinary.com/dfcfolnxk/video/upload/v1777412725/Palya_Kanji_Final_kdg5ap.mp4" },
   { id: 8, title: 'Personal Branding', thumbnail: '/thumbnail/influencers/suja.png', videoUrl: "https://res.cloudinary.com/dfcfolnxk/video/upload/v1777412797/Final_Out_bowo7x.mp4" },
   { id: 9, title: 'Bridal Reels', thumbnail:'/thumbnail/influencers/yazhini.png', videoUrl: "https://res.cloudinary.com/dfcfolnxk/video/upload/v1777412746/Yazhini_Sis_Bridal_ray5yi.mp4" },
-  { id: 10, title: 'Jerina', thumbnail:'/thumbnail/influencers/Jerina.png', videoUrl: "https://res.cloudinary.com/dfcfolnxk/video/upload/v1780844604/Jerina_cqzw5n.mp4" }
 
 ];
